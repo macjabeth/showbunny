a91697398424c18bfbb93cde1e87f21bf469bbc4
Jonathan Macbeth <macbethjonathan@gmail.com>
2018-06-03 14:35:02 -0400

    Set up initial API fetch

---
+++ app.js
@@ -0,0 +1,39 @@
+let searchbox = document.getElementById('search-box');
+let searchbutton = document.getElementById('search-button')
+let searchResults = document.getElementById('search-results')
+
+const API_KEY = 'fa4fa1ba075a48db1aeb756f4343bc23'
+
+function buildResultCard(data, category) {
+  let card = document.createElement('li')
+  let image = data.poster_path ? 'https://image.tmdb.org/t/p/w200' + data.poster_path
+                               : 'http://via.placeholder.com/200x300'
+  let html = `
+    <img src="${image}">
+    <div class="container">
+      <h4><b>${category === 'movie' ? data.title : data.name}</b></h4>
+      <p>${data.overview}</p>
+    </div>
+  `
+
+  card.classList.add('search-result')
+  card.innerHTML = html
+
+  searchResults.appendChild(card)
+}
+
+searchbutton.addEventListener('click', (event) => {
+  event.preventDefault()
+  let text = searchbox.value
+  let category = document.querySelector('input[name="search-category"]:checked').value;
+  let searchQuery = `https://api.themoviedb.org/3/search/${category}?api_key=${API_KEY}&query=${text}`
+
+  fetch(searchQuery)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json)
+      json.results.forEach((result) => {
+        buildResultCard(result, category)
+      })
+    })
+})
