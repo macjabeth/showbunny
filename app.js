976f478f0baf5411730cb851d39a2af572a390a7
Jonathan Macbeth <macbethjonathan@gmail.com>
2018-06-07 00:59:57 -0400

    Add semicolons
    Fixes issue #2

--- app.js
+++ app.js
@@ -1,70 +1,72 @@
 // html elements
-const searchForm = document.getElementById('search-form')
-const searchResults = document.getElementById('search-results')
-const searchPagination = document.getElementById('search-pagination')
-const mediaDialog = document.getElementById('media-dialog')
+const searchForm = document.getElementById('search-form');
+const searchResults = document.getElementById('search-results');
+const searchPagination = document.getElementById('search-pagination');
+const mediaDialog = document.getElementById('media-dialog');
 
 // access data
-const API_KEY = 'fa4fa1ba075a48db1aeb756f4343bc23'
-let searchQuery, searchCategory
+const API_KEY = 'fa4fa1ba075a48db1aeb756f4343bc23';
+let searchQuery, searchCategory;
 
 // event listeners
-searchForm.addEventListener('submit', handleSearch)
+searchForm.addEventListener('submit', handleSearch);
 
 // function handlers
 function handleSearch(event) {
-  const query = document.getElementById('search-bar').value
-  const category = document.querySelector('input[name="search-category"]:checked').value
+  const query = document.getElementById('search-bar').value;
+  const category = document.querySelector('input[name="search-category"]:checked').value;
 
-  fetchData(query, category)
+  fetchData(query, category);
 
-  event.preventDefault()
+  event.preventDefault();
 }
 
 function fetchData(query, category, page = 1) {
   if (query === '') {
-    alert('Bad bunny! You must add a search query.')
+    alert('Bad bunny! You must add a search query.');
   } else {
-    searchQuery = `https://api.themoviedb.org/3/search/${category}?api_key=${API_KEY}&query=${query}&page=${page}`
-    searchCategory = category
+    searchQuery = `https://api.themoviedb.org/3/search/${category}?api_key=${API_KEY}&query=${query}&page=${page}`;
+    searchCategory = category;
     fetch(searchQuery)
       .then((response) => response.json())
       .then((json) => handleSearchResults(json))
-      .catch((reason) => console.error(reason))
+      .catch((reason) => console.error(reason));
   }
 }
 
 fetchData('bunny', 'movie');
 
 function handleSearchResults(json) {
-  console.log(json)
+  console.log(json);
 
   // clear existing search results
-  searchResults.innerHTML = ''
+  while (searchResults.firstChild) {
+    searchResults.removeChild(searchResults.firstChild);
+  }
 
   // display results
   json.results.forEach((result) => {
-    searchResults.appendChild(createResultCard(result))
-  })
+    searchResults.appendChild(createResultCard(result));
+  });
 
-  createPagination(json.total_pages, json.page)
+  createPagination(json.total_pages, json.page);
 }
 
 function createResultCard(result) {
-  const li = document.createElement('li')
+  const li = document.createElement('li');
 
   // set class
-  li.className = 'search-result'
+  li.className = 'search-result';
 
   // create image
-  const img = document.createElement('img')
-  img.src = result.poster_path ? `https://image.tmdb.org/t/p/w200${result.poster_path}` : 'http://via.placeholder.com/200x300'
+  const img = document.createElement('img');
+  img.src = result.poster_path ? `https://image.tmdb.org/t/p/w200${result.poster_path}` : 'http://via.placeholder.com/200x300';
 
   // handle click event - media dialog events will go here
-  li.addEventListener('click', () => console.log(result))
+  li.addEventListener('click', () => console.log(result));
 
   // append the children
-  li.appendChild(img)
+  li.appendChild(img);
 
   // Add title and year info
   const info = document.createElement('div');
@@ -86,7 +88,7 @@ function createResultCard(result) {
 
   li.addEventListener('mouseover', showInfo);
   li.addEventListener('mouseleave', hideInfo);
-  return li
+  return li;
 }
 
 function showInfo() {
@@ -99,44 +101,46 @@ function hideInfo() {
 
 function createPagination(pages, current) {
   // clear existing search pagination
-  searchPagination.innerHTML = ''
+  while (searchPagination.firstChild) {
+    searchPagination.removeChild(searchPagination.firstChild);
+  }
 
   // add previous page
   if (current > 1) {
-    const previousPage = document.createElement('a')
+    const previousPage = document.createElement('a');
 
-    previousPage.innerHTML = '&laquo;'
+    previousPage.innerHTML = '&laquo;';
     previousPage.addEventListener('click', () => {
-      fetchData(searchQuery, searchCategory, current - 1)
-    })
+      fetchData(searchQuery, searchCategory, current - 1);
+    });
 
-    searchPagination.appendChild(previousPage)
+    searchPagination.appendChild(previousPage);
   }
 
   // add all pages
   for (let i = 1; i <= pages; i++) {
-    const link = document.createElement('a')
+    const link = document.createElement('a');
 
-    if (current === i) link.className = 'active'
+    if (current === i) link.className = 'active';
 
-    link.textContent = i
+    link.textContent = i;
 
     link.addEventListener('click', () => {
-      fetchData(searchQuery, searchCategory, i)
-    })
+      fetchData(searchQuery, searchCategory, i);
+    });
 
-    searchPagination.appendChild(link)
+    searchPagination.appendChild(link);
   }
 
   // add next page
   if (current < pages) {
-    const nextPage = document.createElement('a')
+    const nextPage = document.createElement('a');
 
-    nextPage.innerHTML = '&raquo;'
+    nextPage.innerHTML = '&raquo;';
     nextPage.addEventListener('click', () => {
-      fetchData(searchQuery, searchCategory, current + 1)
-    })
+      fetchData(searchQuery, searchCategory, current + 1);
+    });
 
-    searchPagination.appendChild(nextPage)
+    searchPagination.appendChild(nextPage);
   }
 }
