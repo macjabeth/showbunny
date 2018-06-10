24efba3d76ede13c783f8089c39c6c4a39aa5606
Jonathan Macbeth <macbethjonathan@gmail.com>
2018-06-09 22:23:10 -0400

    Include API key

--- bunnychan.js
+++ bunnychan.js
@@ -1,17 +1,16 @@
-// const API_KEY = 'fa4fa1ba075a48db1aeb756f4343bc23';
 class BunnyChan {
-  constructor(key) {
-    this.key = key;
+  constructor() {
+    this.api_key = 'fa4fa1ba075a48db1aeb756f4343bc23';
   }
 
   async createRequestToken() {
-    const response = await fetch(`https://api.themoviedb.org/3/authentication/token/new?api_key=${this.key}`);
+    const response = await fetch(`https://api.themoviedb.org/3/authentication/token/new?api_key=${this.api_key}`);
     const data = await response.json();
     return data;
   }
 
   async createSession(token) {
-    const response = await fetch(`https://api.themoviedb.org/3/authentication/token/new?api_key=${this.key}&request_token=${token}`);
+    const response = await fetch(`https://api.themoviedb.org/3/authentication/token/new?api_key=${this.api_key}&request_token=${token}`);
     const data = await response.json();
     return data;
   }
