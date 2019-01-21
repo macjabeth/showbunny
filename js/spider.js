5d99ffa48b052dbf723c4c2b172e321e494e5455
macjabeth <macbethjonathan@gmail.com>
2019-01-21 21:16:24 +0000

    more variables for colours & darker theme

---
+++ js/spider.js
@@ -0,0 +1,15 @@
+export default class Spider {
+  constructor () {
+    this.key = '4VQ6XG7DQ6o6EhxC';
+  }
+
+  getMovieStream (id) {
+    const url = `https://videospider.in/getvideo?key=${this.key}&video_id=${id}&tmdb=1`;
+    return `<iframe src="${url}" width="100%" height="100%" frameborder="0" allowfullscreen="true" scrolling="no"></iframe>`;
+  }
+
+  getTVStream (id, season, episode) {
+    const url = `https://videospider.in/getvideo?key=${this.key}&video_id=${id}&tmdb=1&tv=1&s=${season}&e=${episode}`;
+    return `<iframe src="${url}" width="100%" height="100%" frameborder="0" allowfullscreen="true" scrolling="no"></iframe>`;
+  }
+}
