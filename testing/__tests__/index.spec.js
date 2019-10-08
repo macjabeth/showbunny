0c509de9892f8b3dccefc008173c9422cf2d885f
macjabeth <macbethjonathan@gmail.com>
2019-10-07 23:42:04 -0400

    update file structure

--- testing/__tests__/index.spec.js
+++ testing/__tests__/index.spec.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
+const html = fs.readFileSync(path.resolve(__dirname, '../public/index.html'), 'utf8');
 const { getByText, getByPlaceholderText, wait } = require('@testing-library/dom');
 require('@testing-library/jest-dom/extend-expect');
 
