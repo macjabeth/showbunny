e162a5794b2aa7756be629db634a53ec51e6093b
Lillian Kodi <lillian.kodi@gmail.com>
2021-10-01 21:23:12 -0400

    CustomElement test

--- js/main.js
+++ js/main.js
@@ -2,6 +2,17 @@ import './vendor/modernizr-3.11.2.min.js';
 import './plugins';
 import './components';
 
+class CustomElement extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({mode:'open'});
+    const template = document.getElementById('custom-element');
+    const clone = template.content.clodeNode(true);
+    shadow.appendChild(clone);
+  }
+}
+customElements.define('custom-element', CustomElement);
+
 // Set up SPA Router
 import Router from './router';
 const router = new Router();
